0397a43a3c8998e2c1ccb443a06e2a8feb240f1a
Devon Taylor <devont049@gmail.com>
2023-11-17 14:14:39 -0600

    started docstrings

---
+++ src/tictactoe.py
@@ -0,0 +1,13 @@
+def get_winner(player_x_location, player_o_location):
+    # determines how to win (three marks in a row)
+
+def winner_status(winner):
+    # state who won or if no winner is present 
+    
+def main():
+    print("Start game!")
+    player_x_location = input
+    player_o_location = input
+
+if __name__ == "__main__":
+    main()
