811ebc2450bbd6a4c46978d9c9f2a6597d403c5d
Devon Taylor <devont049@gmail.com>
2023-11-17 14:21:36 -0600

    more docstrings

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,9 +1,16 @@
+def box_display():
+    # show box template 
+
 def get_winner(player_x_location, player_o_location):
     # determines how to win (three marks in a row)
 
 def winner_status(winner):
     # state who won or if no winner is present 
-    
+
+def confetti_display():
+    # confetti shown dedicated to winner 
+    # no winner = no confetti
+
 def main():
     print("Start game!")
     player_x_location = input
