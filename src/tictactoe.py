4e53cb8c3949eed416fe9b708f8e8b492af52b60
Devon Taylor <devont049@gmail.com>
2023-11-26 21:13:12 -0600

    gameRunning

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -14,7 +14,14 @@ def printBoard(board):
     print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
 printBoard(board)
+
 # take player input 
+def playerInput(board):
+    inp = int(input("Enter a number 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("Oops, player is already in that spot!")
 
 # check for win or tie 
 
@@ -22,5 +29,8 @@ printBoard(board)
 
 # check for win or tie again
 
-#if __name__ == "__main__":
-    #main()
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+    
