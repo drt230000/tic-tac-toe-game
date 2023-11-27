f81983f19afbc248c8416e5bc8d671a63dd48fd1
Devon Taylor <devont049@gmail.com>
2023-11-26 21:34:17 -0600

    checkRow

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -34,8 +34,21 @@ def checkHorizontal(board):
         winner = board[3]
         return True 
     elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
         return True 
+    
 
+def checkRow(board):
+    global winner 
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True 
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
 # switch the player 
 
 # check for win or tie again
